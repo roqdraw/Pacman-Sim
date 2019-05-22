2814eef4a923ce60bfe262d7c7620f46ec635899
roqdraw <roqdraw@gmail.com>
2019-05-22 21:06:49 +1000

    Updates

---
+++ precache-manifest.02a22e5cdfa383b2fc24763baf56012e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3b7c3e26cda45a26e81575f72eea90",
+    "url": "/Pacman-Sim/index.html"
+  },
+  {
+    "revision": "f6610ca0c1347a503d23",
+    "url": "/Pacman-Sim/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/Pacman-Sim/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "f6610ca0c1347a503d23",
+    "url": "/Pacman-Sim/static/js/main.1b3c62fd.chunk.js"
+  },
+  {
+    "revision": "19f751610c363e9f1229",
+    "url": "/Pacman-Sim/static/js/runtime~main.90d4bc2d.js"
+  }
+]);
