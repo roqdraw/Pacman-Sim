e4ed59ec1cb1098505f3481d7a544bc28a4bb35b
roqdraw <roqdraw@gmail.com>
2019-05-23 21:47:38 +1000

    basic instructions as buttons displayed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ function App() {
   return (
     <div className="main">
       <p>PAC-MAN 2: Return of Blinky and Clyde.</p>
+      <div className="game">GAME STARTS HERE.</div>
+      {/* Grid of 5 Squares: x = (0,1,2,3,4), y = (0,1,2,3,4) */}
+      <div>COMMANDS</div>
+      <button>PLACE</button>
+      {/* Always first. */}
+      {/* Type in x co-ord, y co-ord to dictate wher it lands. */}
+      <button>MOVE</button>
+      {/* Moves PAC 1 square where its facing. */}
+      <button>LEFT</button>
+      {/* PAC rotates 90 degrees to the left. */}
+      <button>RIGHT</button>
+      {/* PAC rotates 90 degrees to the right. */}
+      <button>REPORT</button>
+      {/* Outputs x co-ord, y co-ord & whicg cardinal direction its facing. */}
     </div>
   );
 }
